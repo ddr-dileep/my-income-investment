387a91927251cd9d9b45dc6244258837ad7111a8
ddr-dilep <ddr.dileepraloti@gmail.com>
2024-11-05 11:56:55 +0530

    fix : auth layout

--- my-investment/src/app/(pages)/(auth)/layout.tsx
+++ my-investment/src/app/(pages)/(auth)/layout.tsx
@@ -1,9 +1,39 @@
+"use client";
+import Link from "next/link";
 import "./styles.scss";
+import { usePathname } from "next/navigation";
 
 export default function AuthPagesLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="auth-page">{children}</div>;
+  const pathName = usePathname();
+
+  return (
+    <div className="auth-page">
+      {children}
+      <div className="auth-page-footer">
+        <small className="block mt-5">
+          {`${
+            pathName?.includes("register")
+              ? "Already have an account? "
+              : "Don't have an account? "
+          }`}
+          <Link
+            href={`${pathName?.includes("register") ? "/login" : "/register"}`}
+            className="text-blue-700 font-semibold"
+          >
+            {`${pathName?.includes("register") ? "Login" : "Register"}`}
+          </Link>
+        </small>
+        <small>
+          Forgot your password?{" "}
+          <Link href="/forgot-password" className="text-red-800 font-semibold">
+            Reset Password
+          </Link>
+        </small>
+      </div>
+    </div>
+  );
 }
--- my-investment/src/app/(pages)/(auth)/login/page.tsx
+++ my-investment/src/app/(pages)/(auth)/login/page.tsx
@@ -1,3 +1,18 @@
 export default function Login() {
-  return <div className="login-page">Login page</div>;
+  return (
+    <div className="login-page">
+      <h1>Login</h1>
+      {/* <form>
+      <label>
+        Email
+        <input type="email" name="email" required />
+      </label>
+      <label>
+        Password
+        <input type="password" name="password" required />
+      </label>
+      <button type="submit">Login</button>
+    </form> */}
+    </div>
+  );
 }
