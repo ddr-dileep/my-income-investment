6efe167464eab9ca4350a2a911204318754cb6a9
ddr-dilep <ddr.dileepraloti@gmail.com>
2024-11-06 12:24:04 +0530

    fix : user info api integration

--- my-investment/src/app/(pages)/(auth)/login/page.tsx
+++ my-investment/src/app/(pages)/(auth)/login/page.tsx
@@ -4,11 +4,13 @@ import { authAPI } from "@/api/auth";
 import Form from "@/components/Form";
 import Heading from "@/components/Heading";
 import { loginFields } from "@/utils/constants";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Login() {
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
   const onSubmit = async (formValues: object) => {
     setError(null);
@@ -21,11 +23,15 @@ export default function Login() {
       console.log("Login successful:", response);
       if (response?.success === true) {
         localStorage.setItem("token", response?.data?.token);
-        window.location.href = "/dashboard";
+
+        // user info api
+        const resp = await authAPI.userInfo();
+        localStorage.setItem("user-info", JSON.stringify(resp?.data));
+        router.push("/dashboard");
         alert(response.message);
       }
     } catch (err) {
-      console.log(err)
+      console.log(err);
       setError("Failed to log in. Please check your credentials."); // Custom error message
     } finally {
       setLoading(false);
