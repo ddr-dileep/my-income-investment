8f37820f9d7d41a95abb00f177f63e771cd96034
ddr-dilep <ddr.dileepraloti@gmail.com>
2024-11-06 11:38:29 +0530

    feat : login api  integration

--- my-investment/src/app/(pages)/(auth)/login/page.tsx
+++ my-investment/src/app/(pages)/(auth)/login/page.tsx
@@ -1,20 +1,46 @@
 "use client";
+
+import { authAPI } from "@/api/auth";
 import Form from "@/components/Form";
 import Heading from "@/components/Heading";
 import { loginFields } from "@/utils/constants";
+import { useState } from "react";
 
 export default function Login() {
-  const onSubmit = (formValues: object) => {
-    console.log("object is submitted", formValues);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (formValues: object) => {
+    setError(null);
+    setLoading(true);
+
+    try {
+      console.log("Form submitted:", formValues);
+      const response = await authAPI.login(formValues);
+
+      console.log("Login successful:", response);
+      if (response?.success === true) {
+        localStorage.setItem("token", response?.data?.token);
+        window.location.href = "/dashboard";
+        alert(response.message);
+      }
+    } catch (err) {
+      setError("Failed to log in. Please check your credentials."); // Custom error message
+    } finally {
+      setLoading(false);
+    }
   };
+
   return (
     <div className="login-page">
       <Heading
         title="Welcome back"
-        subtitle="fill below form and login"
+        subtitle="Fill below form and login"
         className="text-center"
       />
       <Form fields={loginFields} onSubmit={onSubmit} />
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
   );
 }
